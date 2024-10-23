60dfa95084b029c209eeae0737bb88425b717f98
e-a-mariah <mariah.marques.souza@escola.pr.gov.br>
2024-10-23 10:34:56 -0300

    Update RedesFavoritasMundo.js

--- roleta/RedesFavoritasMundo.js
+++ roleta/RedesFavoritasMundo.js
@@ -1,7 +1,6 @@
 import { getCSS, criarGrafico, incluirTexto } from "./common.js"
 
 async function redesFavoritasMundo() {
-    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/redes-favoritas.json'
     const res = await fetch(url)
     const dados = await res.json()
     const redes = Object.keys(dados)
